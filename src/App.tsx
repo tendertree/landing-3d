1c0073a9c2d646c7c4770e4b665adbc307d4e3ac
tendertree <tendertree@gmail.com>
2023-06-21 01:15:49 +0900

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Pano from "./components/Pano";
+
+function App() {
+
+
+	return (
+		<>
+			<div id="box" className="bg-red-100 w-full h-screen mx-auto items-center justify-center"><Pano></Pano></div>
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/Pano.tsx
@@ -0,0 +1,8 @@
+const Pano = () => {
+
+	return (
+		<div>  <iframe src="/panorama.html" /></div>
+	)
+}
+
+export default Pano;
